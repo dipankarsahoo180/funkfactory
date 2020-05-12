b36f71df894f923ef021a91cf2275bd89fc09895
dipankarsahoo180 <dipankarsahoo180@gmail.com>
2020-05-12 20:01:23 +0530

    Renamed site administration

--- funkfactory/homeapp/urls.py
+++ funkfactory/homeapp/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib import admin
+
+admin.site.site_header = 'Funk Factory Dance Studio'                           # default: "Django Administration"
+admin.site.index_title = 'Funk Factory administration'                           # default: "Site administration"
+admin.site.site_title = 'FUNK FACTORY DANCE STUDIO'
 
 urlpatterns = [
     path('', views.home),
